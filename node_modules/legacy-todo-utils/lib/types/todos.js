51c5e1d1fe0ecca767525c0f051013a9e90748a4
Steve Calvert <scalvert@linkedin.com>
2021-12-01 14:29:47 -0800

    Removing node_modules

--- node_modules/legacy-todo-utils/lib/types/todos.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TodoFileFormat = void 0;
-var TodoFileFormat;
-(function (TodoFileFormat) {
-    TodoFileFormat[TodoFileFormat["Version1"] = 1] = "Version1";
-    TodoFileFormat[TodoFileFormat["Version2"] = 2] = "Version2";
-})(TodoFileFormat = exports.TodoFileFormat || (exports.TodoFileFormat = {}));
-//# sourceMappingURL=todos.js.map
